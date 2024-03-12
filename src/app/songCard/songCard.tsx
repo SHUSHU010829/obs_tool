fec0a99a04811a0a5ca20a331778e07f17ea24bd
FOR-Shu <annie550635@gmail.com>
2024-03-13 00:07:00 +0800

    feat: cd 光盤

--- src/app/songCard/songCard.tsx
+++ src/app/songCard/songCard.tsx
@@ -5,6 +5,12 @@ import { useEffect, useState } from 'react';
 export default function SongCard() {
 
   return (
-    <div></div>
+    <div>
+      <div className="slow-spin inline-block p-24 rounded-full bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500">
+        <div className="p-1 bg-slate-200 bg-opacity-80 rounded-full">
+          <div className="p-4 bg-white rounded-full"></div>
+        </div>
+      </div>
+    </div>
   );
 }
