49c229db854820876b1edb9571a162300596c6c7
FOR-Shu <annie550635@gmail.com>
2024-03-16 16:00:36 +0800

    feat: 更改時鐘背景顏色

--- src/app/clock/clock.tsx
+++ src/app/clock/clock.tsx
@@ -43,7 +43,7 @@ export default function Clock() {
 
   return (
     <div className="flex h-screen items-center justify-center">
-      <div className="py- flex items-center rounded-3xl border-4 border-[#6b705c] bg-[#f9f7f3] bg-opacity-30 px-10 py-6 shadow-lg">
+      <div className="py- flex items-center rounded-3xl border-4 border-[#6b705c] bg-[#f9f7f3] bg-opacity-70 px-10 py-6 shadow-lg">
         <div className="clock__circle">
           <div className="clock__rounder"></div>
           <div
