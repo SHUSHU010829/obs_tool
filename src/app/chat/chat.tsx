526faf14738766703cb3c42528afe3c2ee6b8a46
ShuyuanChuang <shuyuan.chuang@relianz.tw>
2024-03-18 01:50:47 +0800

    fix: chat style

--- src/app/chat/chat.tsx
+++ src/app/chat/chat.tsx
@@ -4,6 +4,8 @@ import { useEffect, useState } from "react";
 
 export default function Chat() {
   return (
-    <div className="flex h-screen items-center justify-center bg-slate-500"></div>
+    <div className="flex h-screen items-center justify-center">
+      <div className=" flex h-[760px] w-[400px] items-center rounded-3xl border-4 border-[#6b705c] bg-[#f9f7f3] bg-opacity-70 px-10 py-6 shadow-lg"></div>
+    </div>
   );
 }
