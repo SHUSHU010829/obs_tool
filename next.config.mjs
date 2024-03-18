042f730c53e236c958cebfcbf1202a32c44baf77
ShuyuanChuang <shuyuan.chuang@relianz.tw>
2024-03-18 15:30:19 +0800

    fix: 新增聊天室背景

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,9 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+    env: {
+    TWITCH_CLIENT_ID: process.env.TWITCH_CLIENT_ID,
+    TWITCH_ACCESS_TOKEN: process.env.TWITCH_ACCESS_TOKEN,
+  },
+};
 
 export default nextConfig;
---
+++ src/api/twitch.ts
@@ -0,0 +1,24 @@
+import axios from "axios";
+
+export async function getStreams() {
+  const config = {
+    headers: {
+      Authorization: process.env.TWITCH_ACCESS_TOKEN,
+      "Client-Id": process.env.TWITCH_CLIENT_ID,
+    },
+  };
+
+  try {
+    const response = await axios.get(
+      "https://api.twitch.tv/helix/streams?user_id=720691521",
+      config
+    );
+    return response.data;
+  } catch (error: unknown) {
+    if (axios.isAxiosError(error)) {
+      throw new Error("Error saving data: " + error.message);
+    } else {
+      throw new Error("Unknown error occurred");
+    }
+  }
+}
